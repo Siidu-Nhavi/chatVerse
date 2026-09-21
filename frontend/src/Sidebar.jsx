2ae0ad0198bd90d883c73306d2e54febb726ea25
Siddu Nhavi <siddunhavi@gmail.com>
2026-09-22 00:39:23 +0530

    Designed: sidebar of application

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -4,11 +4,20 @@ function Sidebar() {
         <section className="sidebar">
            {/* new chat button */}
            <button>
-               <img></img>
-               <i></i>
+               <img src='./src/assets/blacklogo.png' alt="logo" className="logo" />
+              <span><i className="fa-solid fa-pen-to-square" ></i></span>
            </button>
            {/* history */}
+           <ul className="history">
+                <li>history 1</li>
+                <li>history 2</li>
+                <li>history 3</li>
+            </ul>
+
            {/* sign */}
+           <div className="sign">
+            <p>Love by &hearts; siddu</p>
+           </div>
         </section>
     );
 }
