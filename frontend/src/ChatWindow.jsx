d85c148d2416c2a13e4ebec5438e6cc0fbf16081
Siddu Nhavi <siddunhavi@gmail.com>
2026-09-22 23:30:29 +0530

    Designed: The ChatInputBox & NavBar

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,10 +1,36 @@
-import './ChatWindow.css';
+import "./ChatWindow.css";
+import Chat from "./Chat";
 function ChatWindow() {
-    return (
-        <>
-            <h1>Chat Window</h1>
-        </>
-    );
+  return (
+    <div className="chatWindow">
+      <div className="navbar">
+        <span>
+          ChatterBox <i class="fa-solid fa-chevron-down"></i>
+        </span>
+
+        <div className="userIcon">
+          <span className="userIconSpan">
+            <i class="fa-solid fa-user"></i>
+          </span>
+        </div>
+      </div>
+
+      <Chat />
+
+      <div className="chatInput">
+        <div className="userInput">
+          <input placeholder="Ask anything"></input>
+          <div id="submit">
+            <i class="fa-solid fa-arrow-up"></i>
+          </div>
+        </div>
+        <p className="info">
+          ChatterBox can make mistakes.Check important info.See Cookie
+          Preferences.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default ChatWindow;
